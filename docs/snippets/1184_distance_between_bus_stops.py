e212dd602bac42c4a0de36f46f88ee6625a4a14b
SihanA <57210715+russhustle@users.noreply.github.com>
2024-09-16 11:14:25 +0900

    20240916

---
+++ docs/snippets/1184_distance_between_bus_stops.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+# Simulation
+def distanceBetweenBusStops(
+    distance: List[int], start: int, destination: int
+) -> int:
+    if start > destination:
+        start, destination = destination, start
+
+    clock = sum(distance[start:destination])
+    counter = sum(distance) - clock
+
+    return min(clock, counter)
+
+
+distance = [1, 2, 3, 4]
+start = 0
+destination = 1
+print(distanceBetweenBusStops(distance, start, destination))  # 1

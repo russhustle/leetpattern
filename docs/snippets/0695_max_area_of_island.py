328b08f602b934af8e6de8ae1cf5ff473f4dbfc5
russhustle <a_sihan@outlook.com>
2024-10-23 21:52:30 +0900

    20241023

--- docs/snippets/0695_max_area_of_island.py
+++ docs/snippets/0695_max_area_of_island.py
@@ -2,7 +2,7 @@ from collections import deque
 from typing import List
 
 
-# 1. DFS
+# DFS
 def maxAreaOfIslandDFS(grid: List[List[int]]) -> int:
     if not grid:
         return 0
@@ -13,8 +13,10 @@ def maxAreaOfIslandDFS(grid: List[List[int]]) -> int:
 
     def dfs(r, c):
         if (
-            r not in range(m)
-            or c not in range(n)
+            r < 0
+            or r >= m
+            or c < 0
+            or c >= n
             or grid[r][c] == 0
             or (r, c) in visited
         ):
@@ -33,80 +35,86 @@ def maxAreaOfIslandDFS(grid: List[List[int]]) -> int:
     return area
 
 
-# 2. BFS
+# BFS
 def maxAreaOfIslandBFS1(grid: List[List[int]]) -> int:
     if not grid:
         return 0
 
     m, n = len(grid), len(grid[0])
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     visited = set()
-    area = 0
+    res = 0
 
     def bfs(r, c):
         q = deque([(r, c)])
-        subArea = 0
+        area = 0
 
         while q:
             row, col = q.popleft()
-            subArea += 1
+            area += 1
 
-            for dr, dc in directions:
-                nr = row + dr
-                nc = col + dc
+            for dr, dc in dirs:
+                nr, nc = row + dr, col + dc
+
                 if (
-                    nr in range(m)
-                    and nc in range(n)
-                    and grid[nr][nc] == 1
-                    and (nr, nc) not in visited
+                    nr < 0
+                    or nr >= m
+                    or nc < 0
+                    or nc >= n
+                    or grid[nr][nc] == 0
+                    or (nr, nc) in visited
                 ):
-                    visited.add((nr, nc))
-                    q.append((nr, nc))
+                    continue
 
-        return subArea
+                visited.add((nr, nc))
+                q.append((nr, nc))
+
+        return area
 
     for r in range(m):
         for c in range(n):
             if grid[r][c] == 1 and (r, c) not in visited:
                 visited.add((r, c))
-                area = max(area, bfs(r, c))
+                res = max(res, bfs(r, c))
 
-    return area
+    return res
 
 
-# 3. BFS + Grid
+# BFS + Grid
 def numIslandsBFS2(grid: List[List[str]]) -> int:
     if not grid:
         return 0
 
     m, n = len(grid), len(grid[0])
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    area = 0
+    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    res = 0
 
     def bfs(r, c):
         q = deque([(r, c)])
-        subArea = 0
+        area = 0
 
         while q:
             row, col = q.popleft()
-            subArea += 1
+            area += 1
 
-            for dr, dc in directions:
-                nr = row + dr
-                nc = col + dc
-                if nr in range(m) and nc in range(n) and grid[nr][nc] == 1:
-                    q.append((nr, nc))
-                    grid[nr][nc] = 0
+            for dr, dc in dirs:
+                nr, nc = row + dr, col + dc
 
-        return subArea
+                if nr < 0 or nr >= m or nc < 0 or nc >= n or grid[nr][nc] == 0:
+                    continue
+
+                q.append((nr, nc))
+                grid[nr][nc] = 0
+
+        return area
 
     for r in range(m):
         for c in range(n):
             if grid[r][c] == 1:
                 grid[r][c] = 0
-                area = max(area, bfs(r, c))
+                res = max(res, bfs(r, c))
 
-    return area
+    return res
 
 
 grid = [

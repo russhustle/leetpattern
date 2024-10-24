e54035ad76db2580113612bca30b4dd3ad943cd3
russhustle <a_sihan@outlook.com>
2024-10-24 15:31:52 +0900

    20241024

--- docs/snippets/0516_longest_palindromic_subsequence.py
+++ docs/snippets/0516_longest_palindromic_subsequence.py
@@ -1,11 +1,11 @@
 def longestPalindromeSubseq(s: str) -> int:
-    dp = [[0] * len(s) for _ in range(len(s))]
-    for i in range(len(s)):
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+    for i in range(n):
         dp[i][i] = 1
 
-    for i in range(len(s) - 1, -1, -1):
-        for j in range(i + 1, len(s)):
-
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1, n):
             if s[i] == s[j]:
                 dp[i][j] = dp[i + 1][j - 1] + 2
             else:

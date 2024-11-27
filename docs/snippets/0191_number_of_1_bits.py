8bff3fed267748bdc0ae7c3befbe8132b8fc273b
russhustle <a_sihan@outlook.com>
2024-11-27 18:49:32 +0900

    20241127

--- docs/snippets/0191_number_of_1_bits.py
+++ docs/snippets/0191_number_of_1_bits.py
@@ -13,6 +13,26 @@ def hammingWeight2(n: int) -> int:
     return bin(n).count("1")
 
 
+def hammingWeight3(n: int) -> int:
+    def decimalToBinary(n: int) -> str:
+        if n == 0:
+            return "0"
+
+        binary = ""
+        while n > 0:
+            binary = str(n % 2) + binary
+            n //= 2
+
+        return binary
+
+    binary = decimalToBinary(n)
+
+    return binary.count("1")
+
+
 n = 11
 print(hammingWeight1(n))  # 3
 print(hammingWeight2(n))  # 3
+
+n = 47
+print(bin(n))

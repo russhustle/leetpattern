3ab091d7d90da9aa3568c94fb1ef5beb64812f8e
russhustle <a_sihan@outlook.com>
2025-02-02 16:42:33 +0900

    Update

--- utils/sync.py
+++ utils/sync.py
@@ -1,7 +1,8 @@
 import os
+import re
 
 
-def sync_files(directory):
+def sync_sql_files(directory):
     sql_files = {
         os.path.splitext(f)[0]
         for f in os.listdir(directory)
@@ -23,16 +24,63 @@ def sync_files(directory):
         if not os.path.exists(sql_path):
             open(sql_path, "w").close()
             i += 1
-            print(f"Created {sql_path}")
 
         if not os.path.exists(py_path):
             open(py_path, "w").close()
             i += 1
-            print(f"Created {py_path}")
+
+    print(f"Created {i} files")
+
+
+def sync_files(directory):
+    py_folder = os.path.join(directory)
+    cpp_folder = os.path.join(directory, "cpp")
+    cpp_default_file_path = os.path.join("utils", "default.cc")
+    ts_folder = os.path.join(directory, "ts")
+
+    pattern = re.compile(r"^\d{4}_")
+
+    py_files = {
+        os.path.splitext(f)[0]
+        for f in os.listdir(py_folder)
+        if f.endswith(".py") and pattern.match(f)
+    }
+    cpp_files = {
+        os.path.splitext(f)[0]
+        for f in os.listdir(cpp_folder)
+        if f.endswith(".cc") and pattern.match(f)
+    }
+    ts_files = {
+        os.path.splitext(f)[0]
+        for f in os.listdir(ts_folder)
+        if f.endswith(".ts") and pattern.match(f)
+    }
+
+    all_files = py_files.union(cpp_files).union(ts_files)
+
+    i = 0
+    for file in all_files:
+        py_path = os.path.join(py_folder, file + ".py")
+        cpp_path = os.path.join(cpp_folder, file + ".cc")
+        ts_path = os.path.join(ts_folder, file + ".ts")
+
+        if not os.path.exists(py_path):
+            open(py_path, "w").close()
+            i += 1
+
+        if not os.path.exists(cpp_path):
+            with open(cpp_default_file_path, "r") as f:
+                with open(cpp_path, "w") as f2:
+                    f2.write(f.read())
+            i += 1
+
+        if not os.path.exists(ts_path):
+            open(ts_path, "w").close()
+            i += 1
 
     print(f"Created {i} files")
 
 
 if __name__ == "__main__":
-    directory = "src/sql"  # Change this to your folder path
-    sync_files(directory)
+    sync_files("src")
+    sync_sql_files("src/sql")

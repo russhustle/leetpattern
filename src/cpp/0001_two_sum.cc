c59ac450458e196a4d73447d56ab630a8398ef87
russhustle <a_sihan@outlook.com>
2025-02-10 21:29:18 +0900

    Update

--- src/cpp/0001_two_sum.cc
+++ src/cpp/0001_two_sum.cc
@@ -1,8 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> hash;
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end())
+            {
+                return {hash[complement], static_cast<int>(i)};
+            }
+            hash[nums[i]] = i;
+        }
+        return {};
+    }
+};
 
 int main()
 {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+    cout << result[0] << ", " << result[1] << endl;
     return 0;
 }

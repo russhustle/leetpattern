8fcd0bc1f55da85d7c25f381517b3e1779922790
russhustle <a_sihan@outlook.com>
2025-03-03 10:22:21 +0900

    Add new challenge and solutions for Palindrome Partitioning III; update existing challenges and implement prefix sum method for maximum subarray

--- src/0053_maximum_subarray.py
+++ src/0053_maximum_subarray.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-# DP - Kadane
+# DP Kadane
 def maxSubArrayDP(nums: List[int]) -> int:
     dp = [0 for _ in range(len(nums))]
 
@@ -30,6 +30,21 @@ def maxSubArrayGreedy(nums: List[int]) -> int:
     return max_sum
 
 
+# Prefix Sum
+def maxSubArrayPrefixSum(nums: List[int]) -> int:
+    prefix_sum = 0
+    prefix_sum_min = 0
+    res = float("-inf")
+
+    for num in nums:
+        prefix_sum += num
+        res = max(res, prefix_sum - prefix_sum_min)
+        prefix_sum_min = min(prefix_sum_min, prefix_sum)
+
+    return res
+
+
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 print(maxSubArrayDP(nums))  # 6
 print(maxSubArrayGreedy(nums))  # 6
+print(maxSubArrayPrefixSum(nums))  # 6

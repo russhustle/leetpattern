7718049bdd1e754361b355765bf3cf0be64f25d5
russhustle <a_sihan@outlook.com>
2025-09-14 00:32:02 +0900

    feat: add reverse string solution in C++; add tqdm to main

--- leetpattern/core/main.py
+++ leetpattern/core/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from leetpattern import ProblemSetRepository
 import yaml
+from tqdm import tqdm
 
 
 def main():
@@ -9,7 +10,7 @@ def main():
     main_config = config_folder / "main.yaml"
     with main_config.open("r", encoding="utf-8") as file:
         configs = yaml.safe_load(file)
-    for config in configs:
+    for config in tqdm(configs):
         config_name = str(config) + ".yaml"
         config_path = config_folder / config_name
         problem_set = ProblemSetRepository(

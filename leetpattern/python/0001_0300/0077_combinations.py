ec25bc6977a72e93c1d35156ede11a3b66a72498
russhustle <a_sihan@outlook.com>
2026-06-16 12:49:45 +0900

    refactor(0077): rewrite combinations to Solution class with pruning
    - Bump pre-commit hooks: black 26.5.1, isort 9.0.0a3,
      sqlfluff 4.2.2
    - Refactor to Solution class template with backtracking pruning
    - Replace print statements with pytest-compatible test function

--- leetpattern/python/0001_0300/0077_combinations.py
+++ leetpattern/python/0001_0300/0077_combinations.py
@@ -1,33 +1,38 @@
-import itertools
+from itertools import combinations
 from typing import List
 
 
-# Backtracking
-def combine(n: int, k: int) -> List[List[int]]:
-    res = []
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        """Backtracking: O(k * C(n,k)) time, O(k) space.
+        Prune by ensuring remaining elements suffice for k.
+        """
+        res = []
 
-    def backtrack(start, path):
-        if len(path) == k:
-            res.append(path[:])
-            return None
+        def bt(start: int, path: List[int]) -> None:
+            if len(path) == k:
+                res.append(path[:])
+                return
+            for i in range(start, n - (k - len(path)) + 2):
+                path.append(i)
+                bt(i + 1, path)
+                path.pop()
 
-        for i in range(start, n + 1):
-            path.append(i)
-            backtrack(i + 1, path)
-            path.pop()
+        bt(1, [])
+        return res
 
-    backtrack(1, [])
-
-    return res
-
-
-# itertools
-def combineItertools(n: int, k: int) -> List[List[int]]:
-    path = itertools.combinations(range(1, n + 1), k)
-    return path
+    def combineItertools(self, n: int, k: int) -> List[List[int]]:
+        """Itertools: O(k * C(n,k)) time, O(k) space.
+        Leverage stdlib combinations generator.
+        """
+        return [list(c) for c in combinations(range(1, n + 1), k)]
 
 
-print(combine(4, 2))
-# [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
-print(list(combineItertools(4, 2)))
-# [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
+def test_combine():
+    s = Solution()
+    for fn in (s.combine, s.combineItertools):
+        assert sorted(fn(4, 2)) == sorted(
+            [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+        )
+        assert sorted(fn(4, 3)) == sorted([[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]])
+        assert fn(1, 1) == [[1]]
